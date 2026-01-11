284055690c27595bd5baaca10e3fbbc39393c3c9
Saxashitter <nicholas.rickys3@gmail.com>
2026-01-11 17:25:04 -0500

    ehhh almost done

--- src/Lua/modifiers/main/remilia.lua
+++ src/Lua/modifiers/main/remilia.lua
@@ -1,3 +1,7 @@
+-- ======================================================
+-- MODIFIER SETUP
+-- ======================================================
+
 --- @type heistModifier_t
 local modifier = setmetatable({}, FH:returnModifierMetatable())
 
@@ -6,11 +10,194 @@ modifier.description = "It's gonna be a long night..."
 modifier.type = "main"
 modifier.music = "SPFTDP"
 
--- =======================
--- Attack states
--- =======================
+modifier.chaseDelay = TICRATE
+
+-- ======================================================
+-- FREESLOTS / MOBJ + STATE DEFINITIONS
+-- ======================================================
+
+freeslot(
+	"MT_FH_REMILIA_CHASER",
+	"MT_FH_BULLET",
+
+	"S_FH_REMILIA_CHASER",
+	"S_FH_REGULARBULLET",
+	"S_FH_COOLERLOOKINGBULLET",
+	"S_FH_BEAMBULLET",
+	"S_FH_REMIBAT"
+)
+
+mobjinfo[MT_FH_REMILIA_CHASER].radius = 48 * FU
+mobjinfo[MT_FH_REMILIA_CHASER].height = 64 * FU
+mobjinfo[MT_FH_REMILIA_CHASER].flags =
+	MF_NOCLIP | MF_NOCLIPHEIGHT | MF_NOGRAVITY | MF_SPECIAL
+mobjinfo[MT_FH_REMILIA_CHASER].spawnstate = S_FH_REMILIA_CHASER
+
+mobjinfo[MT_FH_BULLET].radius = 8 * FU
+mobjinfo[MT_FH_BULLET].height = 16 * FU
+mobjinfo[MT_FH_BULLET].flags = MF_NOGRAVITY | MF_MISSILE
+mobjinfo[MT_FH_BULLET].spawnstate = S_FH_REGULARBULLET
+mobjinfo[MT_FH_BULLET].speed = 8 * FU
+
+states[S_FH_REMILIA_CHASER] = {
+	sprite = freeslot("SPR_REMI"),
+	frame = A,
+	tics = -1,
+	nextstate = S_FH_REMILIA_CHASER
+}
+
+states[S_FH_REGULARBULLET] = {
+	sprite = freeslot("SPR_RBL2"),
+	frame = FF_FULLBRIGHT,
+	action = A_GhostMe,
+	tics = -1
+}
+
+states[S_FH_COOLERLOOKINGBULLET] = {
+	sprite = freeslot("SPR_RBL1"),
+	frame = FF_FULLBRIGHT,
+	action = A_GhostMe,
+	tics = -1
+}
+
+states[S_FH_BEAMBULLET] = {
+	sprite = freeslot("SPR_RBEM"),
+	frame = FF_FULLBRIGHT,
+	action = A_GhostMe
+}
+
+states[S_FH_REMIBAT] = {
+	sprite = freeslot("SPR_RBAT"),
+	frame = FF_FULLBRIGHT|FF_ANIMATE,
+	action = A_GhostMe,
+	var1 = C,
+	var2 = 3,
+	tics = -1
+}
+
+-- ======================================================
+-- AI HELPER FUNCTIONS
+-- ======================================================
+
+--- @param player player_t
+local function playerIsValid(player)
+	return player and player.valid and player.mo and player.mo.valid and player.mo.health and player.hr and not player.hr.escaped and not player.hr.downed
+end
+
+local function getClosestPlayer(chaser)
+	local target, dist
+
+	for player in players.iterate do
+		if not playerIsValid(player) then continue end
+
+		local d = R_PointToDist2(
+			0, chaser.z,
+			R_PointToDist2(chaser.x, chaser.y, player.mo.x, player.mo.y),
+			player.mo.z
+		)
+
+		if not target or d < dist then
+			target = player
+			dist = d
+		end
+	end
+
+	return target, dist
+end
+
+local function setTarget(chaser, target)
+	chaser.tracer = target
+	modifier:setState(chaser, "bigBulletShoot")
+end
+
+local function chaserMovement(chaser, target)
+	if not modifier:canTarget(chaser) then return end
+	if not chaser.tracer then return end
+	if not chaser.tracer.valid then return end
+	if not playerIsValid(chaser.tracer.player) then return end
+	if not modifier:canMove(chaser) then return end
+
+	local angle = R_PointToAngle2(chaser.x, chaser.y, chaser.tracer.x, chaser.tracer.y)
+	local distXY = 128 * FU
+	local flyZ = 64 * FU -- direction vector to player (center-to-center)
+	local dx = (chaser.tracer.x + P_ReturnThrustX(nil, angle, -distXY)) - chaser.x
+	local dy = (chaser.tracer.y + P_ReturnThrustY(nil, angle, -distXY)) - chaser.y
+	local dz = ((chaser.tracer.z + chaser.tracer.height/2) + flyZ) - (chaser.z + chaser.height/2)
+	local dist = FH:pointTo3DDist(0, 0, 0, dx, dy, dz)
+
+	if dist > 0 then -- normalize
+		local ndx = FixedDiv(dx, dist)
+		local ndy = FixedDiv(dy, dist)
+		local ndz = FixedDiv(dz, dist)
+		local maxSpeed = 24 * (dist > 0 and FixedDiv(dist, 128 * FU) or 0)
+		local desiredMomX = FixedMul(ndx, maxSpeed) local desiredMomY = FixedMul(ndy, maxSpeed)
+		local desiredMomZ = FixedMul(ndz, maxSpeed / 6) -- if mobj is moving backwards from tracer, divide momentum values
+		local curDist = FH:pointTo3DDist(
+			chaser.x,
+			chaser.y,
+			0, -- remiliaChaser.z + remiliaChaser.height / 2,
+			chaser.tracer.x,
+			chaser.tracer.y,
+			0 -- remiliaChaser.tracer.z + remiliaChaser.height / 2
+		)
+		local futureDist = FH:pointTo3DDist(
+			chaser.x + desiredMomX,
+			chaser.y + desiredMomY,
+			0, -- remiliaChaser.z + remiliaChaser.height / 2 + desiredMomZ,
+			chaser.tracer.x,
+			chaser.tracer.y,
+			0 -- remiliaChaser.tracer.z + remiliaChaser.height / 2
+		)
+
+		if curDist < futureDist then
+			local mul = FU / 7
+			desiredMomX = FixedMul($, mul)
+			desiredMomY = FixedMul($, mul)
+			-- desiredMomZ = FixedMul($, mul)
+		end
+
+		chaser.momx = desiredMomX
+		chaser.momy = desiredMomY
+		chaser.momz = desiredMomZ
+	end
+
+	local friction = FU * 10 / 11
+	chaser.momx = FixedMul($, friction)
+	chaser.momy = FixedMul($, friction)
+	chaser.momz = FixedMul($, friction)
+end
+
+-- ======================================================
+-- STATE SYSTEM CORE
+-- ======================================================
+
+function modifier:setState(chaser, stateName)
+	chaser._state = stateName
+	self.attacks[stateName].init(chaser, chaser.tracer)
+end
+
+function modifier:updateState(chaser)
+	local state = self.attacks[chaser._state]
+	if state then
+		state.update(chaser, chaser.tracer)
+	end
+end
+
+function modifier:canTarget(chaser)
+	local state = self.attacks[chaser._state]
+	return not state or state.canTarget ~= false
+end
+
+function modifier:canMove(chaser)
+	local state = self.attacks[chaser._state]
+	return not state or state.canMove ~= false
+end
+
 modifier.attacks = {
 	shoot = {
+		canTarget = true,
+		canMove = true,
+
 		--- @param chaser mobj_t
 		--- @param target mobj_t
 		init = function(chaser, target)
@@ -71,6 +258,9 @@ modifier.attacks = {
 		end
 	},
 	bigBulletShoot = {
+		canTarget = true,
+		canMove = true,
+
 		--- @param chaser mobj_t
 		--- @param target mobj_t
 		init = function(chaser, target)
@@ -129,283 +319,150 @@ modifier.attacks = {
 				end
 			end
 		end
-    },
+	},
+	stunned = {
+		canTarget = false,
+		canMove = false,
+		--- @param chaser mobj_t
+		--- @param target mobj_t
+		init = function(chaser, target)
+			chaser.stunTime = 5 * TICRATE -- duration in tics (0.5 sec)
+		end,
+
+		--- @param chaser mobj_t
+		--- @param target mobj_t
+		update = function(chaser, target)
+			-- freeze movement
+			chaser.momx = 0
+			chaser.momy = 0
+			chaser.momz = 0
+
+			-- countdown
+			chaser.stunTime = $ - 1
+			if chaser.stunTime <= 0 then
+				chaser.stunTime = nil
+				local target = getClosestPlayer(chaser)
+				if target then
+					setTarget(chaser, target.mo)
+				else
+					chaser.tracer = nil
+				end
+			end
+		end
+	}
 }
 
--- =======================
--- State system
--- =======================
---- @param chaser mobj_t
---- @param stateName string
-function modifier:setState(chaser, stateName)
-    chaser._state = stateName
-    self.attacks[stateName].init(chaser, chaser.tracer)
-end
+-- ======================================================
+-- BULLET UTILITY
+-- ======================================================
 
---- @param chaser mobj_t
-function modifier:updateState(chaser)
-	local state = self.attacks[chaser._state]
-	if not state then return end
-
-	state.update(chaser, chaser.tracer)
-end
-
--- =======================
--- Basic AI config
--- =======================
-modifier.chaseDelay = TICRATE
-
-freeslot("MT_FH_REMILIA_CHASER", "S_FH_REMILIA_CHASER")
-freeslot("MT_FH_BULLET")
-freeslot("S_FH_REGULARBULLET", "S_FH_COOLERLOOKINGBULLET", "S_FH_BEAMBULLET", "S_FH_REMIBAT")
-
-mobjinfo[MT_FH_BULLET].radius = 8 * FU
-mobjinfo[MT_FH_BULLET].height = 16 * FU
-mobjinfo[MT_FH_BULLET].flags = MF_NOGRAVITY | MF_MISSILE
-mobjinfo[MT_FH_BULLET].spawnstate = S_FH_REGULARBULLET
-mobjinfo[MT_FH_BULLET].speed = 8 * FU
-
-states[S_FH_REGULARBULLET].sprite = freeslot("SPR_RBL2")
-states[S_FH_REGULARBULLET].frame = FF_FULLBRIGHT
-states[S_FH_REGULARBULLET].action = A_GhostMe
-states[S_FH_REGULARBULLET].tics = -1
-
-states[S_FH_COOLERLOOKINGBULLET].sprite = freeslot("SPR_RBL1")
-states[S_FH_COOLERLOOKINGBULLET].frame = FF_FULLBRIGHT
-states[S_FH_COOLERLOOKINGBULLET].action = A_GhostMe
-states[S_FH_COOLERLOOKINGBULLET].tics = -1
-
-states[S_FH_REMIBAT].sprite = freeslot("SPR_RBAT")
-states[S_FH_REMIBAT].frame = FF_FULLBRIGHT|FF_ANIMATE
-states[S_FH_REMIBAT].action = A_GhostMe
-states[S_FH_REMIBAT].var1 = C
-states[S_FH_REMIBAT].var2 = 3
-states[S_FH_REMIBAT].tics = -1
-
-states[S_FH_BEAMBULLET].sprite = freeslot("SPR_RBEM")
-states[S_FH_BEAMBULLET].frame = FF_FULLBRIGHT
-states[S_FH_BEAMBULLET].action = A_GhostMe
-
-mobjinfo[MT_FH_REMILIA_CHASER].radius = 48 * FU
-mobjinfo[MT_FH_REMILIA_CHASER].height = 64 * FU
-mobjinfo[MT_FH_REMILIA_CHASER].flags = MF_NOCLIP | MF_NOCLIPHEIGHT | MF_NOGRAVITY | MF_SPECIAL
-mobjinfo[MT_FH_REMILIA_CHASER].spawnstate = S_FH_REMILIA_CHASER
-
-states[S_FH_REMILIA_CHASER].sprite = freeslot("SPR_REMI")
-states[S_FH_REMILIA_CHASER].frame = A
-states[S_FH_REMILIA_CHASER].tics = -1
-states[S_FH_REMILIA_CHASER].nextstate = S_FH_REMILIA_CHASER
-
--- =======================
--- Closest player
--- =======================
-local function getClosestPlayer(remiliaChaser)
-    local target
-    local dist
-
-    for player in players.iterate do
-        if not player.mo or not player.mo.health or not player.mo.valid then continue end
-        if not player.hr or player.hr.escaped or player.hr.downed then continue end
-
-        local playerDist = R_PointToDist2(
-            0, remiliaChaser.z,
-            R_PointToDist2(remiliaChaser.x, remiliaChaser.y, player.mo.x, player.mo.y),
-            player.mo.z
-        )
-
-        if not target or playerDist < dist then
-            target = player
-            dist = playerDist
-        end
-    end
-
-    return target, dist
-end
-
--- =======================
--- Init, update, finish
--- =======================
-function modifier:init()
-    local gametype = FH:isMode()
-    local x, y, z
-
-    for mapthing in mapthings.iterate do
-        if mapthing.type == gametype.signpostThing then
-            x, y, z = FH:getMapthingWorldPosition(mapthing)
-        end
-    end
-
-    P_SetupLevelSky(87)
-    for player in players.iterate do
-        P_SetSkyboxMobj(nil, player)
-    end
-
-    local remiliaChaser = P_SpawnMobj(x, y, z, MT_FH_REMILIA_CHASER)
-    FHR.remiliaChaser = remiliaChaser
-    FHR.remiliaChaserDelay = self.chaseDelay
-end
-
-function modifier:update()
-    local remiliaChaser = FHR.remiliaChaser
-
-    if not remiliaChaser or not remiliaChaser.valid then
-        self:init()
-        remiliaChaser = FHR.remiliaChaser
-    end
-
-    if FHR.remiliaChaserDelay then
-        FHR.remiliaChaserDelay = FHR.remiliaChaserDelay - 1
-    end
-end
-
-function modifier:finish()
-    local remiliaChaser = FHR.remiliaChaser
-
-    if remiliaChaser and remiliaChaser.valid then
-        P_RemoveMobj(remiliaChaser)
-    end
-    FHR.remiliaChaser = nil
-end
-
--- =======================
--- Bullet utility
--- =======================
---- @param chaser mobj_t
---- @param point vector3_t
---- @param speed fixed_t|nil
 function modifier:bulletToPoint(chaser, point, speed)
-    local m = P_SpawnMobjFromMobj(chaser, 0, 0, chaser.height / 2 - mobjinfo[MT_FH_BULLET].height / 2, MT_FH_BULLET)
-    if not m or not m.valid then return end
+	local m = P_SpawnMobjFromMobj(
+		chaser, 0, 0,
+		chaser.height/2 - mobjinfo[MT_FH_BULLET].height/2,
+		MT_FH_BULLET
+	)
+	if not m or not m.valid then return end
 
+	local dx = point.x - m.x
+	local dy = point.y - m.y
+	local dz = point.z - (m.z + m.height/2)
+
+	local mag = FH:pointTo3DDist(0,0,0, dx,dy,dz)
+	if mag == 0 then return m end
+
+	dx,dy,dz = FixedDiv(dx,mag), FixedDiv(dy,mag), FixedDiv(dz,mag)
+	speed = speed or m.info.speed
+
+	m.momx = FixedMul(speed, dx)
+	m.momy = FixedMul(speed, dy)
+	m.momz = FixedMul(speed, dz)
+
+	-- randomize frame
 	m.frame = ($ & ~FF_FRAMEMASK)|P_RandomRange(A, P)
 
-    local sx, sy, sz = m.x, m.y, m.z + m.height / 2
-    local dx, dy, dz = point.x - sx, point.y - sy, point.z - sz
-    local mag = FH:pointTo3DDist(0,0,0, dx, dy, dz)
-    if mag == 0 then return m end
-
-    dx, dy, dz = FixedDiv(dx, mag), FixedDiv(dy, mag), FixedDiv(dz, mag)
-    speed = speed or m.info.speed
-
-    m.momx = FixedMul(speed, dx)
-    m.momy = FixedMul(speed, dy)
-    m.momz = FixedMul(speed, dz)
-
-    return m
+	return m
 end
 
-local function setTarget(chaser, target)
-	chaser.tracer = target
-	modifier:setState(chaser, "bigBulletShoot")
-end
+addHook("MobjThinker", function(chaser)
+	if not chaser.valid then return end
+	if FHR.remiliaChaserDelay then return end
 
--- =======================
--- Mobj thinker hook
--- =======================
---- @param remiliaChaser mobj_t
-addHook("MobjThinker", function(remiliaChaser)
-	if not remiliaChaser.valid then return end
-
-	remiliaChaser.shadowscale = FU
-
-	if FHR.remiliaChaserDelay then
-		return
-	end
-
-	-- =======================
-	-- Target acquisition
-	-- =======================
-	if not (remiliaChaser.tracer
-	and remiliaChaser.tracer.valid
-	and remiliaChaser.tracer.health
-	and remiliaChaser.tracer.player
-	and remiliaChaser.tracer.player.hr
-	and not remiliaChaser.tracer.player.hr.downed
-	and not remiliaChaser.tracer.player.hr.escaped) then
-		local target = getClosestPlayer(remiliaChaser)
-		if target then
-			setTarget(remiliaChaser, target.mo)
-		else
-			remiliaChaser.tracer = nil
-		end
-	end
-
-	if remiliaChaser.tracer then
-		local angle = R_PointToAngle2(remiliaChaser.x, remiliaChaser.y, remiliaChaser.tracer.x, remiliaChaser.tracer.y)
-		local distXY = 128 * FU
-		local flyZ = 64 * FU
-
-		-- direction vector to player (center-to-center)
-		local dx = (remiliaChaser.tracer.x + P_ReturnThrustX(nil, angle, -distXY)) - remiliaChaser.x
-		local dy = (remiliaChaser.tracer.y + P_ReturnThrustY(nil, angle, -distXY)) - remiliaChaser.y
-		local dz = ((remiliaChaser.tracer.z + remiliaChaser.tracer.height/2) + flyZ)
-		         - (remiliaChaser.z + remiliaChaser.height/2)
-
-		local dist = FH:pointTo3DDist(0, 0, 0, dx, dy, dz)
-		if dist > 0 then
-			-- normalize
-			local ndx = FixedDiv(dx, dist)
-			local ndy = FixedDiv(dy, dist)
-			local ndz = FixedDiv(dz, dist)
-
-			local maxSpeed = 24 * (dist > 0 and FixedDiv(dist, 128 * FU) or 0)
-			local desiredMomX = FixedMul(ndx, maxSpeed)
-			local desiredMomY = FixedMul(ndy, maxSpeed)
-			local desiredMomZ = FixedMul(ndz, maxSpeed / 4)
-
-			-- if mobj is moving backwards from tracer, divide momentum values
-			local curDist = FH:pointTo3DDist(
-				remiliaChaser.x,
-				remiliaChaser.y,
-				0, -- remiliaChaser.z + remiliaChaser.height / 2,
-				remiliaChaser.tracer.x,
-				remiliaChaser.tracer.y,
-				0 -- remiliaChaser.tracer.z + remiliaChaser.height / 2
-			)
-			local futureDist = FH:pointTo3DDist(
-				remiliaChaser.x + desiredMomX,
-				remiliaChaser.y + desiredMomY,
-				0, -- remiliaChaser.z + remiliaChaser.height / 2 + desiredMomZ,
-				remiliaChaser.tracer.x,
-				remiliaChaser.tracer.y,
-				0 -- remiliaChaser.tracer.z + remiliaChaser.height / 2
-			)
-
-			if curDist < futureDist then
-				local mul = FU / 7
-				desiredMomX = FixedMul($, mul)
-				desiredMomY = FixedMul($, mul)
-				-- desiredMomZ = FixedMul($, mul)
+	if modifier:canTarget(chaser) then
+		if not (chaser.tracer and chaser.tracer.valid and playerIsValid(chaser.tracer.player)) then
+			local target = getClosestPlayer(chaser)
+			if target then
+				setTarget(chaser, target.mo)
+			else
+				chaser.tracer = nil
 			end
-
-			remiliaChaser.momx = desiredMomX
-			remiliaChaser.momy = desiredMomY
-			remiliaChaser.momz = desiredMomZ
 		end
 	end
 
-	local friction = FU * 10 / 11
-	remiliaChaser.momx = FixedMul($, friction)
-	remiliaChaser.momy = FixedMul($, friction)
-	remiliaChaser.momz = FixedMul($, friction)
+	if modifier:canMove(chaser) then
+		chaserMovement(chaser, chaser.tracer)
+	end
 
-	-- =======================
-	-- Attack / state logic
-	-- =======================
-	-- modifier:updateState(remiliaChaser)
+	modifier:updateState(chaser)
 end, MT_FH_REMILIA_CHASER)
 
-addHook("TouchSpecial", function(remiliaChaser, victim)
+-- ======================================================
+-- TOUCH SPECIAL (STUN)
+-- ======================================================
+
+addHook("TouchSpecial", function(chaser, victim)
+	if chaser.hurtTime and leveltime - chaser.hurtTime < 8*TICRATE then
+		return true
+	end
+
+	if not P_PlayerCanDamage(victim.player, chaser)
+	and not victim.fh_instashield then
+		return true
+	end
+
 	victim.momx = -$
 	victim.momy = -$
 
-	--- @type heistOverlay_t
-	local overlay = P_SpawnMobjFromMobj(remiliaChaser, 0,0,0, MT_FH_OVERLAY)
-	overlay.alphaFuse = 10
+	local overlay = P_SpawnMobjFromMobj(chaser, 0,0,0, MT_FH_OVERLAY)
+	overlay.target = chaser
 	overlay.translation = "FH_AllRed"
-	overlay.target = remiliaChaser
+	overlay.alphaFuse = 10
+
+	chaser.hurtTime = leveltime
+	modifier:setState(chaser, "stunned")
 
 	return true
 end, MT_FH_REMILIA_CHASER)
+
+-- ======================================================
+-- MODIFIER LIFECYCLE
+-- ======================================================
+
+function modifier:init()
+	local gametype = FH:isMode()
+	local x,y,z
+
+	for mt in mapthings.iterate do
+		if mt.type == gametype.signpostThing then
+			x,y,z = FH:getMapthingWorldPosition(mt)
+		end
+	end
+
+	FHR.remiliaChaser = P_SpawnMobj(x,y,z, MT_FH_REMILIA_CHASER)
+	FHR.remiliaChaserDelay = self.chaseDelay
+end
+
+function modifier:update()
+	if FHR.remiliaChaserDelay then
+		FHR.remiliaChaserDelay = $ - 1
+	end
+end
+
+function modifier:finish()
+	if FHR.remiliaChaser and FHR.remiliaChaser.valid then
+		P_RemoveMobj(FHR.remiliaChaser)
+	end
+	FHR.remiliaChaser = nil
+end
 
 return FH:registerModifier(modifier)
